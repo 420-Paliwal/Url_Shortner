68564fbc6324e71c3b0f62da8a1649ea97168408
420-Paliwal <harshpaliwal0420@gmail.com>
2025-10-05 23:32:52 +0530

    add new users

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ mongoose.connect('mongodb://localhost:27017/urlShortner', {
 });
 
 app.set('view engine', 'ejs')
-app.set('views', path.resolve('./views'))
+app.set('views', path.resolve('./views'))                   
 
-app.use(express.json())
+app.use(express.json())                                    
 app.use(express.urlencoded({extended: false}))
 app.use('/url', urlRoute)
 app.use('/', staticRoute)
